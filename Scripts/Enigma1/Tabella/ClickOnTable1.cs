fd15ef0a80a956cc4414115cb178eff1603dad14
S1moT3ch <s.cantore2@studenti.poliba.it>
2024-12-09 11:10:37 +0100

    Update ClickOnTable1.cs

--- Scripts/Enigma1/Tabella/ClickOnTable1.cs
+++ Scripts/Enigma1/Tabella/ClickOnTable1.cs
@@ -14,12 +14,12 @@ public class ClickOnTable1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Alpha0) && click) //se si clicca il tasto 0, viene aggiornato nella casella 1 il valore 0, analogo ragionamento si può fare per le i successivi script, che aggiornano le altre caselle.
+        if(Input.GetKeyDown(KeyCode.Alpha0) && click) //se si clicca il tasto 0 e si clicca sul collider, viene aggiornato nella casella 1 il valore 0, analogo ragionamento si può fare per le i successivi script, che aggiornano le altre caselle.
         {
             ModifyOnTable1.istance.UpdateCodiceTabella1("0");
             click = false;
         }
-        else if(Input.GetKeyDown(KeyCode.Alpha1) && click) //se si clicca il tasto 1, viene aggiornato nella casella 1 il valore 1
+        else if(Input.GetKeyDown(KeyCode.Alpha1) && click) //se si clicca il tasto 1 e si clicca sul collider, viene aggiornato nella casella 1 il valore 1
         {
             ModifyOnTable1.istance.UpdateCodiceTabella1("1");
             click = false;
