eaa4097a5d6c1ce1afc1a155b47823490a490244
S1moT3ch <s.cantore2@studenti.poliba.it>
2024-12-09 13:19:48 +0100

    Update PickUpDKit.cs

--- Scripts/Enigma5/DNAKit/PickUpDKit.cs
+++ Scripts/Enigma5/DNAKit/PickUpDKit.cs
@@ -6,18 +6,18 @@ public class PickUpDKit : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        DNAkit.SetActive(true);
+        DNAkit.SetActive(true); //all'inizio, il kit del player è nascosto
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (DKit.istance.clickOn)
+        if (DKit.istance.clickOn) //se si clicca sul collider del kit 1, mostra il kit 1 e aggiorna le varibili
         {
             DNAkit.SetActive(false);
             DKit.istance.clickOn = false;
         }
-        else if (DKit2.istance.clickOn)
+        else if (DKit2.istance.clickOn) //altrimenti se si clicca sul collider del kit2, mostra il kit 2 e aggiorna le variabili
         {
             DNAkit.SetActive(false);
             DKit.istance.clickOn = false;
